e6513e923f53cddc03f8637c0a1d86e5f308b4fa
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-10-06 00:33:31 -0300

    creating task boards

--- src/views/ProjectStepsView/ProjectStepsView.jsx
+++ src/views/ProjectStepsView/ProjectStepsView.jsx
@@ -1,46 +1,48 @@
 import React, { useState } from "react";
-import { Tabs, Typography, Steps } from "antd";
+import { Tabs, Typography, Steps, Button, message } from "antd";
 import { useTranslation } from "react-i18next";
+import ControlledBoard from "components/Project/ControlledBoard";
 
 const { Step } = Steps;
 
 const ProjectStepsView = () => {
   const { t } = useTranslation();
+  const [currentStep, setCurrentStep] = useState(1);
 
   const steps = [
     {
-      title: 'First',
-      content: 'First-content',
+      key: 1,
+      title: "project_steps_view.step_1.title",
+      description: "project_steps_view.step_1.description",
+      content: "First-content",
     },
     {
-      title: 'Second',
-      content: 'Second-content',
+      key: 2,
+      title: "project_steps_view.step_2.title",
+      description: "project_steps_view.step_2.description",
+      content: "Second-content",
     },
     {
-      title: 'Last',
-      content: 'Last-content',
+      key: 3,
+      title: "project_steps_view.step_3.title",
+      description: "project_steps_view.step_3.description",
+      content: "Third-content",
     },
   ];
 
   return (
-    <Steps current={1}>
-      <Step
-        key="1"
-        title={t("project_steps_view.step_1.title")}
-        description={t("project_steps_view.step_1.description")}
-      />
-      <Step
-        key="2"
-        title={t("project_steps_view.step_2.title")}
-        subTitle={t("project_steps_view.step_2.sub_title")}
-        description={t("project_steps_view.step_2.description")}
-      />
-      <Step
-        key="3"
-        title={t("project_steps_view.step_3.title")}
-        description={t("project_steps_view.step_3.description")}
-      />
-    </Steps>
+    <>
+      <Steps current={currentStep} onChange={(curr) => setCurrentStep(curr)}>
+        {steps.map((item) => (
+          <Step
+            key={item.title}
+            title={t(item.title)}
+            description={t(item.description)}
+          />
+        ))}
+      </Steps>
+      <div>{steps[currentStep].content}</div>
+    </>
   );
 };
 
