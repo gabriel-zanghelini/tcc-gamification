897f3907439e5243683de2f25a3ce814e5046ee8
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-10-25 23:37:18 -0300

    create popover for reputation points tag

--- src/components/Common/RepPointsTag/RepPointsTag.jsx
+++ src/components/Common/RepPointsTag/RepPointsTag.jsx
@@ -1,21 +1,70 @@
 import React from "react";
-import { Icon, Tag } from "antd";
+import { Alert, Icon, List, Popover, Tag, Tooltip } from "antd";
 
-const RepPointsTag = ({ points, action }) => {
+const PopoverContent = ({ infoMsg, alertMsg }) => {
+  const alertStyle = { marginBottom: 10 };
+  return (
+    <div>
+      {infoMsg ? (
+        <Alert showIcon type="info" message={infoMsg} style={alertStyle} />
+      ) : null}
+      {alertMsg ? (
+        <Alert showIcon type="warning" message={alertMsg} style={alertStyle} />
+      ) : null}
+    </div>
+  );
+};
+
+const RepPointsTag = ({ points, action, style, color }) => {
   let icon =
     action === "plus"
-      ? "up-circle"
+      ? "double-left"
       : action === "minus"
-      ? "down-circle"
+      ? "double-right"
       : "crown";
+
+  let plusContent = (
+    <PopoverContent
+      infoMsg={`Você irá ganhar ${points} pontos ao realizar essa ação.`}
+      alertMsg={`Você precisa ter ao menos ${points} de Reputação para realizar essa ação.`}
+    />
+  );
+
+  let minusContent = (
+    <PopoverContent
+      infoMsg={null}
+      alertMsg={`Você irá perder ${points} pontos se realizar essa ação.`}
+    />
+  );
+
+  let defaultContent = (
+    <PopoverContent infoMsg={`${points} Pontos de Reputação`} alertMsg={null} />
+  );
+
+  let tooltipText =
+    action === "plus"
+      ? plusContent
+      : action === "minus"
+      ? minusContent
+      : defaultContent;
+
   return (
-    <Tag color="#FF7043" style={{ marginRight: "0" }}>
-      <span>
-        {points}
-        &nbsp;
-        <Icon type={icon} />
-      </span>
-    </Tag>
+    <Popover content={tooltipText}>
+      <Tag
+        color={color ? color : "#FF7043"}
+        style={{ marginRight: "0", pointerEvents: "all", ...style }}
+      >
+        <span>
+          {points}
+          &nbsp;
+          <Icon
+            type={icon}
+            style={{ color: "#fff" }}
+            rotate={action ? 90 : 0}
+          />
+        </span>
+      </Tag>
+    </Popover>
   );
 };
 
