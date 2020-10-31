4750728d456530678c045055692d5b1a31ad0a21
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-10-30 23:48:45 -0300

    add pontuation routes

--- server/api/index.js
+++ server/api/index.js
@@ -4,9 +4,11 @@ import registerProject from "./project";
 import registerLogin from "./login";
 import registerUser from "./user";
 import registerTask from "./task";
+import registerPontuation from "./pontuation";
 
 const router = Router();
 
+registerPontuation(router);
 registerProject(router);
 registerLogin(router);
 registerUser(router);
