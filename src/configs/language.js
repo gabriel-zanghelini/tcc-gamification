ca706f0a3d426cd54046c9e5b3584549585d8117
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-11-02 00:16:52 -0300

    add date format

--- src/configs/language.js
+++ src/configs/language.js
@@ -5,6 +5,21 @@ import LanguageDetector from "i18next-browser-languagedetector";
 
 export const DEFAULT_LANGUAGE = "en-US";
 export const LANGUAGES = ["en-US", "pt-BR"];
+export const EN_DATE_FORMAT = "MM/DD/YYYY";
+export const PT_DATE_FORMAT = "DD/MM/YYYY";
+
+export const getDateString = (date) => {
+  let day = date.getDate() > 9 ? date.getDate() : "0" + date.getDate();
+  let month =
+    date.getMonth() > 8 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1);
+  let year = date.getFullYear();
+
+  if (i18n.language === "pt-BR") {
+    return `${day}/${month}/${year}`;
+  } else {
+    return `${month}/${day}/${year}`;
+  }
+};
 
 i18n
   .use(Backend)
@@ -18,18 +33,18 @@ i18n
     cleanCode: true,
 
     interpolation: {
-      escapeValue: false
+      escapeValue: false,
     },
 
     detection: {
       order: ["localStorage", "navigator"],
       caches: ["localStorage"],
-      checkWhitelist: true
+      checkWhitelist: true,
     },
 
     react: {
-      wait: true
-    }
+      wait: true,
+    },
   });
 
 export default i18n;
