f2814028e320217b31b2c144eb833ad2f6345bc7
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-11-02 23:28:20 -0300

    added progress bar to level border

--- src/components/MainLayout/UserDrawer/UserDrawer.jsx
+++ src/components/MainLayout/UserDrawer/UserDrawer.jsx
@@ -1,50 +1,15 @@
 import React from "react";
 
-import { Drawer, Avatar, Divider, Typography } from "antd";
+import { Drawer } from "antd";
 import { observer } from "mobx-react";
 
 import { useTranslation } from "react-i18next";
-import useCurrentUserStore from "stores/CurrentUserStore";
 
 import * as Styled from "./styled";
-import RepPointsTag from "components/Common/RepPointsTag";
-import StarterBorder from "../ReputationBorders/StarterBorder";
-import GoldBorder from "../ReputationBorders/GoldBorder";
-import SilverBorder from "../ReputationBorders/SilverBorder";
-import BronzeBorder from "../ReputationBorders/BronzeBorder";
-import PlatinumBorder from "../ReputationBorders/PlatinumBorder";
-import DiamondBorder from "../ReputationBorders/DiamondBorder";
-
-const { Text, Title } = Typography;
-
-const ProfileBorder = ({ children }) => {
-  const currentUserStore = useCurrentUserStore();
-  const reputationBorders = {
-    0: StarterBorder,
-    250: BronzeBorder,
-    750: SilverBorder,
-    1500: GoldBorder,
-    3000: PlatinumBorder,
-    5000: DiamondBorder,
-  };
-
-  let Border = StarterBorder;
-
-  Object.keys(reputationBorders).forEach((b) => {
-    let repPoints = currentUserStore.currentUser.reputation_points;
-
-    console.log(repPoints, parseInt(b));
-    if (repPoints >= parseInt(b)) {
-      Border = reputationBorders[b];
-    }
-  });
-  console.log("border", Border);
-  return <Border>{children}</Border>;
-};
+import ProfileBorder from "../ProfileBorder";
 
 const UserDrawer = ({ visible, onClose }) => {
   const { t } = useTranslation();
-  const currentUserStore = useCurrentUserStore();
 
   return (
     <Drawer
@@ -56,30 +21,7 @@ const UserDrawer = ({ visible, onClose }) => {
       style={{ display: "block", margin: "auto" }}
     >
       <Styled.Title>{t("user_drawer.profile")}</Styled.Title>
-      <ProfileBorder>
-        <div style={{ width: 285 }}>
-          <Styled.Avatar>
-            <Avatar
-              size={64}
-              style={{ backgroundColor: "#7265e6", verticalAlign: "middle" }}
-              children={currentUserStore.currentUser?.name?.charAt(0)}
-            />
-          </Styled.Avatar>
-          <Divider />
-          <Styled.Division>
-            <Title level={3}>{currentUserStore.currentUser?.name}</Title>
-          </Styled.Division>
-          <Styled.Division>
-            <Text type="secondary">{currentUserStore.currentUser?.email}</Text>
-          </Styled.Division>
-          <Styled.Division>
-            <RepPointsTag
-              style={{ fontSize: 16, padding: 3, marginTop: 30 }}
-              points={currentUserStore.currentUser?.reputation_points}
-            />
-          </Styled.Division>
-        </div>
-      </ProfileBorder>
+      <ProfileBorder />
     </Drawer>
   );
 };
--- src/components/MainLayout/UserDrawer/styled.js
+++ src/components/MainLayout/UserDrawer/styled.js
@@ -12,22 +12,6 @@ export const SubTitle = styled.h2`
   font-size: 24px;
 `;
 
-export const Avatar = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-export const Division = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  font-size: 18px;
-`;
-
-export const Entry = styled.div`
-  grid-column: span ${({ span }) => span || 2};
-`;
-
 export const Label = styled.span`
   color: var(--black-highlight);
   margin-right: var(--sm-pad);
