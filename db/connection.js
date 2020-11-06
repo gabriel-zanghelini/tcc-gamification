78ed2c2f21f36dfe78f93416572fffdd48df76c1
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-11-06 00:37:48 -0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -22,11 +22,6 @@ const pool =
         database: process.env.DATABASE,
       })
     : new Pool({
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        host: process.env.HOST,
-        port: process.env.PORT,
-        database: process.env.DATABASE,
         connectionString: process.env.DATABASE_URL,
       });
 
