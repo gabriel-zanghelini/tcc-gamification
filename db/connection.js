2ebeefacf0e4b5351f82211ca9df134d1f6f5462
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-11-05 23:41:45 -0300

    fix heroku db connection

--- db/connection.js
+++ db/connection.js
@@ -1,27 +1,34 @@
 const { Pool } = require("pg");
 require("dotenv/config");
 
-console.log("ENV", process.env.NODE_ENV, process.env.DATABASE_URL)
+console.log("ENV", process.env.NODE_ENV, process.env.DATABASE_URL);
 
 console.log(
   "PG_DATA:",
-  process.env.PG_USER,
-  process.env.PG_PASSWORD,
-  process.env.PG_HOST,
-  process.env.PG_PORT,
-  process.env.PG_DATABASE
+  process.env.USER,
+  process.env.PASSWORD,
+  process.env.HOST,
+  process.env.PORT,
+  process.env.DATABASE
 );
 
 const pool =
   process.env.NODE_ENV === "development"
     ? new Pool({
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        host: process.env.PG_HOST,
-        port: process.env.PG_PORT,
-        database: process.env.PG_DATABASE,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        database: process.env.DATABASE,
       })
-    : new Pool({ connectionString: process.env.DATABASE_URL });
+    : new Pool({
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        database: process.env.DATABASE,
+        connectionString: process.env.DATABASE_URL,
+      });
 
 // postgres://gafjlouxgkynoz:f4ed7bdb3fdc1c035d1a6398227fd187b99314f58a7745d18e095b809e5ae9f5@ec2-100-25-100-81.compute-1.amazonaws.com:5432/d6jro4q0k1cg7h
 
