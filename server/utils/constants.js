a7e9c6586728920bbfe1048542b22cbbcaf17f5f
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-11-10 00:37:02 -0300

    Update constants.js

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -1,4 +1,4 @@
-import os from "os";
+// import os from "os";
 import path from "path";
 
 import cryptoRandomString from "crypto-random-string";
@@ -8,19 +8,19 @@ export const ENV = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 8080;
 export const HOST = "0.0.0.0";
 
-const interfaces = os.networkInterfaces();
+// const interfaces = os.networkInterfaces();
 
-let CURRENT_IP = HOST;
-if (CURRENT_IP === "0.0.0.0" || CURRENT_IP === "127.0.0.1") {
-  for (const [iface] of Object.values(interfaces)) {
-    if (iface.family === "IPv4" && !iface.internal) {
-      CURRENT_IP = iface.address;
-      break;
-    }
-  }
-}
+// let CURRENT_IP = HOST;
+// if (CURRENT_IP === "0.0.0.0" || CURRENT_IP === "127.0.0.1") {
+//   for (const [iface] of Object.values(interfaces)) {
+//     if (iface.family === "IPv4" && !iface.internal) {
+//       CURRENT_IP = iface.address;
+//       break;
+//     }
+//   }
+// }
 
-export const IP = CURRENT_IP;
+// export const IP = CURRENT_IP;
 
 export const TOKEN_KEY = cryptoRandomString({ length: 124, type: "base64" });
 export const TOKEN_NAME = "access-token";
